21ac6de14545fdbd1e1855c30a1b73e8d89f5a9d
Chen Xu <cxu.self@gmail.com>
2024-08-23 22:56:50 +0300

    refactor: Use react-query for fetching and displaying products on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,29 @@
-import { useEffect, useState } from "react"
-import { Button } from "../components/ui/button"
 import api from "../api"
+import { useQuery } from "@tanstack/react-query"
 
 import { Product } from "../types"
 
 export function Home() {
-  const [products, setProducts] = useState<Product[]>([])
-  useEffect(() => {
-    const handleFetch = async () => {
-      const res = await api.get("/products")
-      setProducts(res.data)
+  const handleFetchProducts = async () => {
+    const res = await api.get("/products")
+    if (res.status !== 200) {
+      throw new Error("Error fetching products")
     }
-    handleFetch()
-  }, [])
+    return res.data
+  }
+
+  const { data: products, isLoading } = useQuery<Product[]>({
+    queryKey: ["products"],
+    queryFn: handleFetchProducts
+  })
 
   return (
     <div className="flex flex-col justify-center items-center gap-10 h-screen">
       <h1 className="text-2xl">Welcome!</h1>
-      {products.map((product) => (
+      {isLoading && <p>Loading...</p>}
+      {products?.map((product) => (
         <div key={product.id}>
-          <h3>{product.name}</h3>
+          <h4>{product.name}</h4>
         </div>
       ))}
     </div>
