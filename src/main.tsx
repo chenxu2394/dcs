f56aa768e3363e03ed0a09a5d50d68ac852cdb01
Chen Xu <cxu.self@gmail.com>
2024-08-24 11:04:58 +0300

    feat: Add react-query-devtools for debugging and monitoring queries

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import App from "./App"
 import "./index.css"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
