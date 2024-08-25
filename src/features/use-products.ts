d453070b3a26647b1eb34b34372c91840493992d
Chen Xu <cxu.self@gmail.com>
2024-08-25 17:26:11 +0300

    refactor: Extract useCreateProduct hook for adding a new product

--- src/features/use-products.ts
+++ src/features/use-products.ts
@@ -1,6 +1,6 @@
 import api from "@/api"
 import { Product } from "@/types"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 const handleFetchProducts = async () => {
   const res = await api.get("/products")
@@ -17,4 +17,31 @@ export function useGetProducts(): [Product[], boolean] {
     initialData: []
   })
   return [products, isLoading]
+}
+
+const handleAddProduct = async () => {
+  const res = await api.post("/products", {
+    name: "Jeans",
+    description: "This is a new product",
+    quantity: 10,
+    price: 100,
+    discount: 0,
+    categoryId: "de8a2af1-86b3-4471-a1dd-e52ab8d21f9c"
+  })
+
+  // if (res.status !== 200) {
+  //   throw new Error("Error posting product")
+  // }
+
+  return res.data
+}
+export function useCreateProduct() {
+  const queryClient = useQueryClient()
+  const mutation = useMutation<Product>({
+    mutationFn: handleAddProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] })
+    }
+  })
+  return mutation
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,14 @@
-import api from "../api"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-
-import { Product } from "../types"
 import { ProductList } from "@/components/ProductList"
 import { Button } from "@/components/ui/button"
-import { useGetProducts } from "@/features/use-products"
+import { useGetProducts, useCreateProduct } from "@/features/use-products"
 
 export function Home() {
-  const queryClient = useQueryClient()
   const [products, isLoading] = useGetProducts()
-
-  const handleAddProduct = async () => {
-    const res = await api.post("/products", {
-      name: "Jeans",
-      description: "This is a new product",
-      quantity: 10,
-      price: 100,
-      discount: 0,
-      categoryId: "de8a2af1-86b3-4471-a1dd-e52ab8d21f9c"
-    })
-
-    console.log("%csrc/pages/home.tsx:23 res", "color: #007acc;", res)
-    // if (res.status !== 200) {
-    //   throw new Error("Error posting product")
-    // }
-
-    queryClient.invalidateQueries({ queryKey: ["products"] })
-    return res.data
-  }
-
-  const mutation = useMutation<Product[]>({
-    mutationFn: handleAddProduct
-  })
+  const addProduct = useCreateProduct()
 
   return (
     <div className="p-2">
-      <Button onClick={() => mutation.mutate()}>Add Product</Button>
+      <Button onClick={() => addProduct.mutate()}>Add Product</Button>
       {isLoading ? <p>Loading...</p> : <ProductList products={products} />}
     </div>
   )
