6c0af8982cd1673968604c73db65052ed9376ee3
Chen Xu <cxu.self@gmail.com>
2024-08-25 16:51:08 +0300

    refactor: Add "Add Product" button to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,37 @@
 import api from "../api"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 import { Product } from "../types"
 import { ProductList } from "@/components/ProductList"
+import { Button } from "@/components/ui/button"
 
 export function Home() {
+  const queryClient = useQueryClient()
+
   const handleFetchProducts = async () => {
     const res = await api.get("/products")
     if (res.status !== 200) {
       throw new Error("Error fetching products")
     }
+    return res.data
+  }
+
+  const handleAddProduct = async () => {
+    const res = await api.post("/products", {
+      name: "Jeans",
+      description: "This is a new product",
+      quantity: 10,
+      price: 100,
+      discount: 0,
+      categoryId: "de8a2af1-86b3-4471-a1dd-e52ab8d21f9c"
+    })
+
+    console.log("%csrc/pages/home.tsx:23 res", "color: #007acc;", res)
+    // if (res.status !== 200) {
+    //   throw new Error("Error posting product")
+    // }
+
+    queryClient.invalidateQueries({ queryKey: ["products"] })
     return res.data
   }
 
@@ -19,7 +41,14 @@ export function Home() {
     initialData: []
   })
 
+  const mutation = useMutation<Product[]>({
+    mutationFn: handleAddProduct
+  })
+
   return (
-    <div className="p-2">{isLoading ? <p>Loading...</p> : <ProductList products={products} />}</div>
+    <div className="p-2">
+      <Button onClick={() => mutation.mutate()}>Add Product</Button>
+      {isLoading ? <p>Loading...</p> : <ProductList products={products} />}
+    </div>
   )
 }
