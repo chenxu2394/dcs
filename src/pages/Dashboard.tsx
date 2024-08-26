0aa0ab4be3ad4538402c1716ade78bcc9a0a6a9f
Chen Xu <cxu.self@gmail.com>
2024-08-26 15:34:11 +0300

    feat: Move addProduct to the dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,27 @@
+import { Can } from "@/components/Can"
+import { useCreateProduct } from "@/features/use-products"
+import { ProductCreate } from "@/types"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+
+export function Dashboard() {
+  const [newProduct, setNewProduct] = useState<ProductCreate>({
+    name: "Jeans",
+    description: "This is a new product",
+    quantity: 10,
+    price: 100,
+    discount: 0,
+    categoryId: "de8a2af1-86b3-4471-a1dd-e52ab8d21f9c"
+  })
+  const addProduct = useCreateProduct()
+
+  return (
+    <div>
+      <Can
+        permission="PRODUCT:ADD"
+        permissionType="actions"
+        yes={() => <Button onClick={() => addProduct.mutate(newProduct)}>Add Product</Button>}
+      />
+    </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,29 +1,12 @@
 import { ProductList } from "@/components/ProductList"
-import { Button } from "@/components/ui/button"
-import { useGetProducts, useCreateProduct } from "@/features/use-products"
-import { ProductCreate } from "@/types"
-import { useState } from "react"
+import { useGetProducts } from "@/features/use-products"
 import { Can } from "@/components/Can"
 
 export function Home() {
-  const [newProduct, setNewProduct] = useState<ProductCreate>({
-    name: "Jeans",
-    description: "This is a new product",
-    quantity: 10,
-    price: 100,
-    discount: 0,
-    categoryId: "de8a2af1-86b3-4471-a1dd-e52ab8d21f9c"
-  })
   const [products, isLoading] = useGetProducts()
-  const addProduct = useCreateProduct()
 
   return (
     <div className="p-2">
-      <Can
-        permission="PRODUCT:ADD"
-        permissionType="actions"
-        yes={() => <Button onClick={() => addProduct.mutate(newProduct)}>Add Product</Button>}
-      />
       <Can
         permission="PRODUCT:GET"
         permissionType="actions"
