7736e71f9953fed0737947d35f69e8703999fa3f
Chen Xu <cxu.self@gmail.com>
2024-08-26 18:59:48 +0300

    refactor: Update NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,33 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport
+  NavigationMenuList
 } from "@/components/ui/navigation-menu"
+
+const NavMenuItemLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  return (
+    <NavigationMenuLink
+      className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+      href={href}
+    >
+      {children}
+    </NavigationMenuLink>
+  )
+}
 
 export function NavBar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="flex justify-between p-2">
+      <NavigationMenu className="flex justify-center">
+        <NavigationMenuList>
+          <NavigationMenuItem className="flex">
+            <NavMenuItemLink href="/">Home</NavMenuItemLink>
+            <NavMenuItemLink href="/products">Products</NavMenuItemLink>
+            <NavMenuItemLink href="/login">Login</NavMenuItemLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   )
 }
