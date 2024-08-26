8bd4889e1408e35a78eb7e2974a89bec75607eda
Chen Xu <cxu.self@gmail.com>
2024-08-26 17:04:12 +0300

    refactor: Customize OneProduct page

--- src/pages/OneProduct.tsx
+++ src/pages/OneProduct.tsx
@@ -1,18 +1,22 @@
 import { useGetOneProduct } from "@/features/use-products"
 import { useParams } from "react-router-dom"
-import { Card, CardDescription, CardTitle } from "@/components/ui/card"
+import { Card, CardDescription, CardFooter, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 export function OneProduct() {
   const { id = "" } = useParams<{ id: string }>()
   const [product, isLoading] = useGetOneProduct(id)
   return (
-    <div className="p-2 justify-center">
+    <div className="flex justify-center min-h-fit">
       {isLoading ? (
         <p>Loading...</p>
       ) : product ? (
-        <Card>
-          <CardTitle>{product.name}</CardTitle>
-          <CardDescription>{product.description}</CardDescription>
+        <Card className="p-2 w-1/2">
+          <CardTitle className="p-2">{product.name}</CardTitle>
+          <CardDescription className="p-2">{product.description}</CardDescription>
+          <CardFooter className="p-2 justify-end">
+            <Button>Add to Card</Button>
+          </CardFooter>
         </Card>
       ) : (
         <Card>
