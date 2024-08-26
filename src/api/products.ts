5f9090c193954e6b34deccf821b8e413a78ac19e
Chen Xu <cxu.self@gmail.com>
2024-08-26 19:44:32 +0300

    refactor: Update getAll method in products API to return validated products

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,13 +1,19 @@
-import { ProductCreate } from "@/types"
+import { Product, ProductCreate, productsSchema } from "@/types"
 import api from "."
 
 const RESOURCE = "/products"
 
 export default {
-  getAll: async () => {
+  getAll: async (): Promise<Product[]> => {
     const res = await api.get(RESOURCE)
     if (res.status !== 200) {
       throw new Error("Error fetching products")
+    }
+
+    const validatedProducts = productsSchema.safeParse(res.data)
+    console.log("%csrc/api/products.ts:14 validatedProducts", "color: #007acc;", validatedProducts)
+    if (!validatedProducts.success) {
+      return []
     }
     return res.data
   },
