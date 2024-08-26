2d7a4212c2c1bf4c7ab24dbe2b7db2ed330504c0
Chen Xu <cxu.self@gmail.com>
2024-08-26 19:44:26 +0300

    refactor: Update product schema and types

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,5 +1,11 @@
 import { z } from "zod"
 
+const categorySchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string()
+})
+
 export const productSchema = z.object({
   id: z.string(),
   name: z.string(),
@@ -7,12 +13,23 @@ export const productSchema = z.object({
   quantity: z.number().min(0),
   price: z.number().min(0),
   discount: z.number().min(0).max(100),
-  categoryId: z.string()
+  category: categorySchema
 })
 export type Product = z.infer<typeof productSchema>
 
-export const productCreateSchema = productSchema.omit({ id: true })
+export const productCreateSchema = productSchema
+  .omit({
+    id: true,
+    category: true
+  })
+  .extend({
+    categoryId: z.string()
+  })
+
 export type ProductCreate = z.infer<typeof productCreateSchema>
 
 export const productUpdateSchema = productSchema.partial()
 export type ProductUpdate = z.infer<typeof productUpdateSchema>
+
+export const productsSchema = z.array(productSchema)
+export type Products = z.infer<typeof productsSchema>
