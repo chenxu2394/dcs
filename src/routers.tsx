b08f60bcd33a536ddf93643de9179269e8294aab
Chen Xu <cxu.self@gmail.com>
2024-08-26 22:35:38 +0300

    refactor: Update import statement for Home component in routers.tsx

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "./App"
-import { Home } from "./pages/home"
+import { Home } from "./pages/Home"
 import { Products } from "./pages/Products"
 import { Dashboard } from "./pages/Dashboard"
 import { OneProduct } from "./pages/OneProduct"
