870d86b9438a6f955583207834781d0a9d983814
Chen Xu <cxu.self@gmail.com>
2024-08-26 21:34:05 +0300

    feat: Add dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { RouterProvider } from "react-router-dom"
 import router from "./routers"
+import { ThemeProvider } from "./components/theme-provider"
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </ThemeProvider>
 )
