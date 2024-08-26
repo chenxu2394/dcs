35a53e787da15f19b597da8119623bdf8bf052ed
Chen Xu <cxu.self@gmail.com>
2024-08-26 20:18:24 +0300

    refactor: Update product API to validate and return products

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,4 +1,4 @@
-import { Product, ProductCreate, productsSchema } from "@/types"
+import { Product, ProductCreate, productSchema, productsSchema } from "@/types"
 import api from "."
 
 const RESOURCE = "/products"
@@ -11,26 +11,36 @@ export default {
     }
 
     const validatedProducts = productsSchema.safeParse(res.data)
-    console.log("%csrc/api/products.ts:14 validatedProducts", "color: #007acc;", validatedProducts)
     if (!validatedProducts.success) {
       return []
     }
     return res.data
   },
 
-  getOne: async (id: string) => {
+  getOne: async (id: string): Promise<Product> => {
     const res = await api.get(`${RESOURCE}/${id}`)
     if (res.status !== 200) {
       throw new Error("Error fetching product")
     }
+
+    const validatedProduct = productSchema.safeParse(res.data)
+    if (!validatedProduct.success) {
+      throw new Error("Error validating product")
+    }
+
     return res.data
   },
 
-  createOne: async (product: ProductCreate) => {
+  createOne: async (product: ProductCreate): Promise<Product> => {
     const res = await api.post(RESOURCE, product)
 
     if (res.status !== 200) {
       throw new Error("Error posting product")
+    }
+
+    const validatedProduct = productSchema.safeParse(res.data)
+    if (!validatedProduct.success) {
+      throw new Error("Error validating product")
     }
 
     return res.data
