2a640b243b4a805f93e0384e251ed9bcb6204e47
Chen Xu <cxu.self@gmail.com>
2024-08-27 21:59:54 +0300

    refactor: Update getSearchQueryKey function to include optional category, minPrice, and maxPrice parameters

--- src/features/utils.ts
+++ src/features/utils.ts
@@ -5,6 +5,19 @@ export function getQueryKey(QUERY_KEY: string, id?: string) {
   return [QUERY_KEY]
 }
 
-export function getSearchQueryKey(QUERY_KEY: string, searchTerm: string, category?: string) {
-  return [QUERY_KEY, "search", searchTerm + (category ? `-${category}` : "")]
+export function getSearchQueryKey(
+  QUERY_KEY: string,
+  searchTerm: string,
+  category?: string,
+  minPrice?: number,
+  maxPrice?: number
+) {
+  return [
+    QUERY_KEY,
+    "search",
+    searchTerm +
+      (category ? `-${category}` : "") +
+      (minPrice ? `-${minPrice}` : "") +
+      (maxPrice ? `-${maxPrice}` : "")
+  ]
 }
