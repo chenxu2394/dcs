bae43ba9b3af76f2f55d86b1c98209d42d283709
Chen Xu <cxu.self@gmail.com>
2024-08-30 00:12:34 +0300

    feat: Add user context in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import { Products } from "@/pages/Products"
+import { UserContext } from "@/providers/user-provider"
+import { useContext } from "react"
 
 export function Home() {
+  const context = useContext(UserContext)
+  if (!context) {
+    return <div>Loading...</div>
+  }
+  console.log("%csrc/pages/Home.tsx:10 context", "color: #007acc;", context)
+
   return <Products />
 }
