006f599889e35d1cec8dee371c7d8a558866c2f0
Chen Xu <cxu.self@gmail.com>
2024-08-29 21:30:37 +0300

    refactor: Replace ternary operator with early return -
    regarding https://github.com/Integrify-Finland/fs18_java_frontend/pull/8#discussion_r1735949453

--- src/pages/OneProduct.tsx
+++ src/pages/OneProduct.tsx
@@ -6,23 +6,28 @@ import { Button } from "@/components/ui/button"
 export function OneProduct() {
   const { id = "" } = useParams<{ id: string }>()
   const [product, isLoading] = useGetOneProduct(id)
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (product === undefined) {
+    return (
+      <Card>
+        <CardTitle>Product not found</CardTitle>
+      </Card>
+    )
+  }
+
   return (
     <div className="flex justify-center min-h-fit">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : product ? (
-        <Card className="p-2 w-1/2">
-          <CardTitle className="p-2">{product.name}</CardTitle>
-          <CardDescription className="p-2">{product.description}</CardDescription>
-          <CardFooter className="p-2 justify-end">
-            <Button>Add to Card</Button>
-          </CardFooter>
-        </Card>
-      ) : (
-        <Card>
-          <CardTitle>Product not found</CardTitle>
-        </Card>
-      )}
+      <Card className="p-2 w-1/2">
+        <CardTitle className="p-2">{product.name}</CardTitle>
+        <CardDescription className="p-2">{product.description}</CardDescription>
+        <CardFooter className="p-2 justify-end">
+          <Button>Add to Card</Button>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
