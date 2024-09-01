3e24c1c84cc3f213fe9522c262cdd8fed296b03f
Chen Xu <cxu.self@gmail.com>
2024-09-01 12:56:09 +0300

    feat: Add users API module for login and user details retrieval

---
+++ src/api/users.ts
@@ -0,0 +1,30 @@
+import { RetrievedUserDetail, UserLoginType } from "@/types"
+import api from "."
+
+const RESOURCE = "/users"
+
+export default {
+  login: async (credentials: UserLoginType): Promise<string> => {
+    const res = await api.post(`${RESOURCE}/login`, credentials)
+    if (res.status !== 200) {
+      throw new Error("Error logging in")
+    }
+
+    // console.log(res.data)
+    return res.data
+  },
+  getUserDetails: async (userId: string): Promise<RetrievedUserDetail> => {
+    if (userId) {
+      const res = await api.get(`${RESOURCE}/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      if (res.status !== 200) {
+        throw new Error("Error fetching user")
+      }
+      return res.data
+    }
+    throw Error("Did not fetch user info")
+  }
+}
