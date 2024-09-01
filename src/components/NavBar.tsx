df56625d163fb2caed9858b8cd33dc96001112cc
Chen Xu <cxu.self@gmail.com>
2024-09-01 15:15:17 +0300

    refactor: Update NavBar to include logout functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,18 +7,23 @@ import {
 import { ModeToggle } from "./mode-toggle"
 import { Link } from "react-router-dom"
 import { Can } from "./Can"
+import { useContext } from "react"
+import { TokenAndDecodedTokenContext } from "@/providers/token-provider"
 
 export const NavMenuItemLink = ({
   href,
+  onClick,
   children
 }: {
   href: string
+  onClick?: () => void
   children: React.ReactNode
 }) => {
   return (
     <NavigationMenuLink
       className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
       href={href}
+      onClick={onClick}
     >
       {children}
     </NavigationMenuLink>
@@ -26,6 +31,12 @@ export const NavMenuItemLink = ({
 }
 
 export function NavBar() {
+  const { removeTokenAndDecodedToken } = useContext(TokenAndDecodedTokenContext)
+
+  function handleLogout() {
+    removeTokenAndDecodedToken()
+  }
+
   return (
     <div className="flex justify-between items-center w-full p-2 shadow-md">
       <Link to="/" className="ml-4 font-bold text-lg no-underline">
@@ -40,6 +51,15 @@ export function NavBar() {
               permission="LOGIN:VIEW"
               permissionType="views"
               yes={() => <NavMenuItemLink href="/login">Login</NavMenuItemLink>}
+            />
+            <Can
+              permission="LOGOUT:VIEW"
+              permissionType="views"
+              yes={() => (
+                <NavMenuItemLink href="/" onClick={handleLogout}>
+                  Logout
+                </NavMenuItemLink>
+              )}
             />
             <NavMenuItemLink href="/dashboard">Dashboard</NavMenuItemLink>
           </NavigationMenuItem>
