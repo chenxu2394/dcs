5fe1133b495f9878ce26abc95f793830e458a617
Chen Xu <cxu.self@gmail.com>
2024-09-04 12:20:38 +0300

    refactor: Update CategorySelector component to allow null value for selectedCategory

--- src/components/CategorySelector.tsx
+++ src/components/CategorySelector.tsx
@@ -15,7 +15,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 
 interface Props {
   allCategoryNames: string[]
-  selectedCategory: string
+  selectedCategory: string | null
   setSelectedCategory: (category: string) => void
   allCategories: boolean
 }
