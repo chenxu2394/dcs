48b157f27b94cc1040b9be75b02e7c17436871b5
Chen Xu <cxu.self@gmail.com>
2024-09-07 18:46:51 +0300

    refactor: Add form validation for user registration credentials

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,6 +4,8 @@ import { Input } from "../components/ui/input"
 import { Button } from "../components/ui/button"
 
 import { useRegister } from "@/features/use-users"
+import { userRegisterSchema } from "@/types"
+import { useToast } from "@/components/ui/use-toast"
 
 export function Register() {
   const [credentials, setCredentials] = useState({
@@ -12,9 +14,24 @@ export function Register() {
     name: ""
   })
   const mutation = useRegister()
+  const { toast } = useToast()
+  const [errors, setErrors] = useState<{ [key: string]: string }>({})
 
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault()
+    const result = userRegisterSchema.safeParse(credentials)
+    if (!result.success) {
+      const errorMessages = result.error.errors.reduce((acc, error) => {
+        acc[error.path[0]] = error.message
+        toast({
+          description: error.message
+        })
+        return acc
+      }, {} as { [key: string]: string })
+      setErrors(errorMessages)
+      return
+    }
+    setErrors({})
     mutation.mutate(credentials)
   }
 
@@ -33,14 +50,23 @@ export function Register() {
         <h3 className="text-3xl">Please Register</h3>
         <div className="p-2">
           <form onSubmit={handleLogin} className="w-full space-y-6">
-            <Input name="email" placeholder="Your email" onChange={handleChange} />
-            <Input name="name" placeholder="Your name" onChange={handleChange} />
-            <Input
-              name="password"
-              placeholder="Your password"
-              type="password"
-              onChange={handleChange}
-            />
+            <div>
+              <Input name="email" placeholder="Your email" onChange={handleChange} />
+              {errors.email && <span className="text-red-500">{errors.email}</span>}
+            </div>
+            <div>
+              <Input name="name" placeholder="Your name" onChange={handleChange} />
+              {errors.name && <span className="text-red-500">{errors.name}</span>}
+            </div>
+            <div>
+              <Input
+                name="password"
+                placeholder="Your password"
+                type="password"
+                onChange={handleChange}
+              />
+              {errors.password && <span className="text-red-500">{errors.password}</span>}
+            </div>
             <Button type="submit" className="w-full">
               Register
             </Button>
