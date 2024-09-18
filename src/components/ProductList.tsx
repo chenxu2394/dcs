a077b80647e31b691b9fb2f7a479267544db652f
Chen Xu <cxu.self@gmail.com>
2024-09-18 20:30:32 +0300

    Refactor ProductList layout to use a responsive grid

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -7,10 +7,12 @@ type ProductListProps = {
 
 export function ProductList({ products }: ProductListProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-5">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
