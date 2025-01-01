65d640f9de5a1d6800cc3e34d12b04ef5bfc6b60
Chen Xu <cxu.self@gmail.com>
2025-01-01 16:04:29 +0800

    chore(api): update baseURL for backend server

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,8 @@ let baseURL = "http://localhost:8080/api"
 if (!isDevelopment) {
   // Update this later when you have a working backend server
   // baseURL = "https://fs18-java-backend-tzc4.onrender.com/api"
-  baseURL = "https://dcstest.azurewebsites.net/api"
+  // baseURL = "https://dcstest.azurewebsites.net/api"
+  baseURL = "https://dcs-backend.azurewebsites.net/api"
 }
 
 const api = axios.create({
